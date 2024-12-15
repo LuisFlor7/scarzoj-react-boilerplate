a65a951e39ffddba607e49fac797b56b29918911
Luis Flor Chacón <luisflorchacon7@gmail.com>
2024-12-15 19:44:48 +0000

    PreLogin y Login realizados

--- src/pages/PreLogin.jsx
+++ src/pages/PreLogin.jsx
@@ -8,6 +8,10 @@ import card2 from "./178.png";
 import card3 from "./122.png";
 import card4 from "./34.png";
 import card5 from "./141.png";
+import card6 from "./155.png";
+import card7 from "./170.png";
+import card8 from "./175.png";
+import card9 from "./177.png";
 import logo from "./Marca Hidden Diamond sin fondo.png";
 
 const PreLogin = () => {
@@ -26,6 +30,10 @@ const PreLogin = () => {
         <img src={card3} alt="Carta 3" className="card card3" />
         <img src={card4} alt="Carta 4" className="card card4" />
         <img src={card5} alt="Carta 5" className="card card5" />
+        <img src={card6} alt="Carta 6" className="card card6" />
+        <img src={card7} alt="Carta 7" className="card card7" />
+        <img src={card8} alt="Carta 8" className="card card8" />
+        <img src={card9} alt="Carta 9" className="card card9" />
       </div>
 
       {/* Logo */}
